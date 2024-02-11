9aaf7c6f0d86f81a63ed7f2f086f517c56a578f4
Yoann Far <yofardev@gmail.com>
2024-02-11 10:37:26 +0100

    first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,81 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
+
+import 'image_editor_page.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final String imageLink =
+      'https://images.unsplash.com/photo-1707159432991-ac67eace0014?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHx8';
+  File? _edited;
+  File? _edited2;
+  File? _original;
+
+  @override
+  void initState() {
+    super.initState();
+    _networkToFile();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SingleChildScrollView(
+      child: Column(
+        children: [
+          TextButton(
+              onPressed: () => _toEditor(_edited!.path),
+              child: const Text('Edit')),
+          TextButton(
+            onPressed: () {
+              setState(() {
+                _edited = _original;
+              });
+            },
+            child: const Text("Reset"),
+          ),
+          if (_original == null) const CircularProgressIndicator(),
+          if (_edited != null) Image.file(_edited!),
+          if (_edited2 != null) Image.file(_edited2!),
+        ],
+      ),
+    ));
+  }
+
+  void _networkToFile() async {
+    final response = await http.get(Uri.parse(imageLink));
+    final bytes = response.bodyBytes;
+    final dir = await getTemporaryDirectory();
+    final file = File('${dir.path}/image.jpg');
+    await file.writeAsBytes(bytes);
+    setState(() {
+      _original = file;
+    });
+    if (mounted) {
+      _toEditor(file.path);
+    }
+  }
+
+  void _toEditor(String path) async {
+    dynamic edited = await Navigator.of(context).push(
+      MaterialPageRoute<dynamic>(
+        builder: (BuildContext context) => ImageEditorPage(imagePath: path),
+      ),
+    );
+    if (edited != null && edited is Map<String, dynamic>) {
+      setState(() {
+        _edited = edited['image'];
+        _edited2 = edited['image2'];
+      });
+    }
+  }
+}
---
+++ lib/image_editor_page.dart
@@ -0,0 +1,165 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:image/image.dart' as img;
+import 'package:path_provider/path_provider.dart';
+import 'package:screenshot/screenshot.dart';
+
+class ImageEditorPage extends StatefulWidget {
+  final String imagePath;
+
+  const ImageEditorPage({
+    Key? key,
+    required this.imagePath,
+  }) : super(key: key);
+
+  @override
+  ImageEditorPageState createState() => ImageEditorPageState();
+}
+
+class ImageEditorPageState extends State<ImageEditorPage> {
+  double _sliderBrightnessValue = 0;
+  final ScreenshotController _screenshotController = ScreenshotController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(),
+      body: Stack(
+        children: <Widget>[
+          Screenshot(
+            controller: _screenshotController,
+            child: ColorFiltered(
+              colorFilter: ColorFilter.matrix(
+                ColorFilterGenerator.brightnessAdjustMatrix(
+                    value: _sliderBrightnessValue),
+              ),
+              child: Image.file(
+                File(widget.imagePath),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          _tools(),
+        ],
+      ),
+    );
+  }
+
+  Widget _tools() => Row(
+        children: [
+          Expanded(
+            child: Slider(
+              value: _sliderBrightnessValue,
+              min: -0.5,
+              max: 2,
+              onChanged: (double value) {
+                setState(() {
+                  _sliderBrightnessValue = value;
+                });
+              },
+            ),
+          ),
+          TextButton(onPressed: _applyBrightness, child: Text("Apply")),
+        ],
+      );
+
+  Future<void> _applyBrightness() async {
+    img.Image? image =
+        img.decodeImage(File(widget.imagePath).readAsBytesSync());
+    if (image == null) return;
+
+    double brightness = convert();
+
+    img.adjustColor(image, brightness: brightness);
+
+    print("brightness: $brightness // $_sliderBrightnessValue");
+
+    final dir = await getTemporaryDirectory();
+    final file = File(
+        '${dir.path}/image_edited_${DateTime.now().toIso8601String()}.jpg');
+    File(file.path).writeAsBytesSync(img.encodeJpg(image));
+
+    final file2 = File(
+        '${dir.path}/image_edited_${DateTime.now().toIso8601String()}.jpg');
+
+    var bytes = await _takeScreenshot();
+    file2.writeAsBytesSync(bytes!);
+
+    Map<String, dynamic> data = {
+      "image": file,
+      "image2": file2,
+    };
+
+    if (mounted) Navigator.of(context).pop(data);
+  }
+
+  Future<Uint8List?> _takeScreenshot() async {
+    var bytes = await _screenshotController.capture();
+    return bytes;
+  }
+
+  double convert() {
+    return _sliderBrightnessValue * 3;
+  }
+}
+
+class ColorFilterGenerator {
+  static List<double> brightnessAdjustMatrix({required double value}) {
+    if (value <= 0) {
+      value = value * 255;
+    } else {
+      value = value * 100;
+    }
+
+    if (value == 0) {
+      return [
+        1,
+        0,
+        0,
+        0,
+        0,
+        0,
+        1,
+        0,
+        0,
+        0,
+        0,
+        0,
+        1,
+        0,
+        0,
+        0,
+        0,
+        0,
+        1,
+        0,
+      ];
+    }
+
+    return List<double>.from(<double>[
+      1,
+      0,
+      0,
+      0,
+      value,
+      0,
+      1,
+      0,
+      0,
+      value,
+      0,
+      0,
+      1,
+      0,
+      value,
+      0,
+      0,
+      0,
+      1,
+      0
+    ]).map((i) => i.toDouble()).toList();
+  }
+}
